f225bbcbe56cdb8e651d6b4aa051ae04e3141ac4
kschecht <kschechter@rocketmail.com>
2023-09-29 18:47:02 -0400

    Removed comments

--- bing_rewards_search.py
+++ bing_rewards_search.py
@@ -5,8 +5,6 @@ import webbrowser
 import re
   
 
-#forbidden_words=["fu","shi","bit","as","por","pus","nu","ti","cu","bu","bb","vi","pen","va","cli","co","se","boo","bre","ora","hen","nak","lab","ji","di","ana","br","sl","dru","mas","wan","les","ho","linge","hand","inc","ama","blo","three","miss","vo","test","spe","ni","bab","gir","squ","ns","liv","bon","ori","rom","dy","play","bj","hj","lol","mur","fac","lai"]
-
 with open("D:\\DocumentsD\\Programming\Python\\bing_rewards\\forbidden_words.txt") as file:
     forbidden_words = [line.rstrip() for line in file]
 
@@ -17,20 +15,7 @@ def is_forbidden(word: str):
             return True
     return False
 
-# match = re.search(r'portal', 'GeeksforGeeks: A computer science \
-#                   portal for geeks')
-# print(match)
-# print(match.group())
-  
-# print('Start Index:', match.start())
-# print('End Index:', match.end())
-
 word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
-
-# with open("newline.txt", "r") as file: 
-
-# crimefile = open(fileName, 'r')
-# yourResult = [line.split(',') for line in crimefile.readlines()]
 
 response = urlopen(word_site)
 txt = response.read()
